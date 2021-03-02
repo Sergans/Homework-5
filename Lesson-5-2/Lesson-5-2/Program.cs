ecd64ac6b3b60c60596a3ccbdb8119b566135756
Сергей <gan7er@yandex.ru>
2021-03-02 18:31:13 +0300

    Начало

---
+++ Lesson-5-2/Lesson-5-2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lesson_5_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filename = "startup.txt";
+            File.WriteAllText(filename, "Текущая дата и время");
+        }
+    }
+}
