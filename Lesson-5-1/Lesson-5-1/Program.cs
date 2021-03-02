cc41eb3b08344d6ec24c2ca9f60c6b1e74086e05
Сергей <gan7er@yandex.ru>
2021-03-02 18:25:24 +0300

    Вариант

---
+++ Lesson-5-1/Lesson-5-1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lesson_5_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filename = "text.txt";
+            File.WriteAllText(filename, Console.ReadLine());
+            
+            
+        }
+    }
+}
