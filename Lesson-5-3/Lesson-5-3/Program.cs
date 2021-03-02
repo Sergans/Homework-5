8cf8734b50ad870e135abbe304758c689fbcf55f
Сергей <gan7er@yandex.ru>
2021-03-02 18:41:40 +0300

    Начало

---
+++ Lesson-5-3/Lesson-5-3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_5_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите числа от 0 до 255 через пробел");
+            string nom = Console.ReadLine();
+
+            string[] nomber = nom.Split(' ');
+            byte[] bin = new byte[nomber.Length];
+
+            for (int i = 0; i < nomber.Length; i++)
+            {
+                bin[i] = Convert.ToByte(nomber[i]);
+
+
+            }
+        }
+    }
+}
